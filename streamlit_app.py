eca0b1e8f2f1286dda907ce3c58915124ce41f8a
kmponcet <116293262+kmponcet@users.noreply.github.com>
2022-10-21 17:00:03 -0400

    Update streamlit_app.py
    organized code & stopped running the snowflake stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +30,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #normalize json response
@@ -34,9 +37,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it as table
 streamlit.dataframe(fruityvice_normalized)
 
-#get fruit list from snowflake
-import snowflake.connector
+#don't run anything here while we troubleshoot
+streamlit.stop()
 
+#get fruit list from snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
